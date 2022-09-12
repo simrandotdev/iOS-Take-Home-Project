08da3699907c0e575c187f0289b2f5f4199df23b
Simran Preet Singh <simran5590@gmail.com>
2022-09-12 08:51:54 -0700

    Initial Commit

---
+++ iOS Take Home Project/iOS_Take_Home_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Take_Home_ProjectApp.swift
+//  iOS Take Home Project
+//
+//  Created by Simran Preet Narang on 2022-09-12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Take_Home_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

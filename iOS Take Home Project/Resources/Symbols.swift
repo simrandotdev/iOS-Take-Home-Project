d31a9bd1e4a1caca01927bdd5f7e4ef4724da9e0
Simran Preet Singh <simran5590@gmail.com>
2022-09-12 09:07:23 -0700

    New project setup with Symbols and Theme

---
+++ iOS Take Home Project/Resources/Symbols.swift
@@ -0,0 +1,16 @@
+//
+//  Symbols.swift
+//  iOS Take Home Project
+//
+//  Created by Simran Preet Narang on 2022-09-12.
+//
+
+import SwiftUI
+
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+    static let link = Image(systemName: "linkcx")
+}
---
+++ iOS Take Home Project/Resources/Theme.swift
@@ -0,0 +1,16 @@
+//
+//  Theme.swift
+//  iOS Take Home Project
+//
+//  Created by Simran Preet Narang on 2022-09-12.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background =  Color("background")
+    static let detailBackground =  Color("detail-background")
+    static let text =  Color("text")
+    static let pill =  Color("pill")
+}
+
